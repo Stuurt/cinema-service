3c7d83a3b6d4cbf65c628a18c9d3b3419cc34dd8
Renato <renaato.d.m@gmail.com>
2024-05-22 01:06:26 -0300

    feature: creat seat entity

--- src/main/java/com/cinema/service/entity/Seat.java
+++ src/main/java/com/cinema/service/entity/Seat.java
@@ -3,12 +3,16 @@ package com.cinema.service.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@AllArgsConstructor
+@NoArgsConstructor
 public class Seat {
     @Id
     private Long id;
     private Integer seatNumber;
-    
+    private Boolean status;
 }

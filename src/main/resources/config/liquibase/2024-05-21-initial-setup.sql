bf0c4395eaec3cbd2064cfce3e9b2546dc6b4faa
Renato <renaato.d.m@gmail.com>
2024-05-23 01:57:29 -0300

    hotfix: database structure

--- src/main/resources/config/liquibase/2024-05-21-initial-setup.sql
+++ src/main/resources/config/liquibase/2024-05-21-initial-setup.sql
@@ -19,27 +19,30 @@ CREATE TABLE room (
     total_seats SMALLINT NOT NULL
 );
 
-CREATE TABLE seat (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    seat_number SMALLINT NOT NULL,
-    status BOOLEAN NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    room_id BIGINT,
-    CONSTRAINT fk_room_id FOREIGN KEY (room_id) REFERENCES room (id)
-);
-
 CREATE TABLE session (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     time_session TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     movie_id BIGINT,
     room_id BIGINT,
+    base_price NUMERIC(10, 2),
     CONSTRAINT fk_movie_id FOREIGN KEY (movie_id) REFERENCES movie (id),
     CONSTRAINT fk_room_id FOREIGN KEY (room_id) REFERENCES room (id)
 );
 
 CREATE TABLE ticket (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    price NUMERIC(10, 2),
-    session_id BIGINT,
-    CONSTRAINT fk_session_id FOREIGN KEY (session_id) REFERENCES session (id)
+                    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                    uuid VARCHAR(36) NOT NULL,
+                    price_paid NUMERIC(10,2) NOT NULL,
+                    discount_type VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE seat (
+                      id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                      seat_number SMALLINT NOT NULL,
+                      status BOOLEAN NOT NULL,
+                      type VARCHAR(255) NOT NULL,
+                      session_id BIGINT,
+                      ticket_id BIGINT,
+                      CONSTRAINT fk_session_id FOREIGN KEY (session_id) REFERENCES session (id),
+                      CONSTRAINT fk_ticket_id FOREIGN KEY (ticket_id) REFERENCES ticket (id)
 );

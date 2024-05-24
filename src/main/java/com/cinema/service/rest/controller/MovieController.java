2d26fc31aaedfb4e4ea1592b6de0cbfbcf93b21b
Renato <renaato.d.m@gmail.com>
2024-05-24 02:40:55 -0300

    feat: controller advice

--- src/main/java/com/cinema/service/rest/controller/MovieController.java
+++ src/main/java/com/cinema/service/rest/controller/MovieController.java
@@ -1,9 +1,13 @@
-package com.cinema.service.rest.controller;
+package com.cinema.service.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/movies")
@@ -12,6 +16,7 @@ public class MovieController {
     @ResponseStatus(code = HttpStatus.ACCEPTED)
     @Operation(summary = "Get movies", description = "bar", parameters = {@Parameter(name = "lelelel")})
     public String getMethodName(@RequestParam String param) {
-            return new String();
+        return new String();
     }
+
 }

054b72c3cc9745c956abe23ac24342c194027026
Renato <renaato.d.m@gmail.com>
2024-05-25 02:15:13 -0300

    session time validation improve

--- src/main/java/com/cinema/service/domain/service/SessionService.java
+++ src/main/java/com/cinema/service/domain/service/SessionService.java
@@ -8,6 +8,7 @@ import com.cinema.service.domain.enums.SeatTypeEnum;
 import com.cinema.service.rest.dto.CreateSessionRequest;
 import com.cinema.service.domain.repository.SessionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,13 +22,18 @@ public class SessionService {
     private final RoomService roomService;
     private final MovieService movieService;
 
+    @Value("${cinema.max-session-hours}")
+    private Integer maxSessionHours;
+    @Value("${cinema.min-session-minutes}")
+    private Integer minimumSessionMinutes;
+
     public Session createSession(CreateSessionRequest session, Long movieId, Long roomId) {
         Movie movie = movieService.findById(movieId);
         Room room = roomService.findById(roomId);
         List<Seat> seats = createSeatsForSession(room.getTotalSeats());
 
-        if (!isRoomEmptyAtThisTime(session.getSessionTime(), session.getSessionEndTime(), roomId))
-            throw new IllegalArgumentException("this room already have a session at this time period");
+        checkRoomAvailability(session.getSessionTime(), session.getSessionEndTime(), roomId);
+        validateSessionDuration(session);
 
         return sessionRepository.save(
                 new Session(
@@ -41,12 +47,31 @@ public class SessionService {
                 ));
     }
 
-    private Boolean isRoomEmptyAtThisTime(
+    private void checkRoomAvailability(
             LocalDateTime sessionTime,
             LocalDateTime sessionEndTime,
             Long roomId) {
 
-        return sessionRepository.isRoomEmptyAtThisTime(sessionTime, sessionEndTime, roomId);
+        Boolean roomIsEmptyAtThisTime = sessionRepository.isRoomEmptyAtThisTime(sessionTime, sessionEndTime, roomId);
+        if (!roomIsEmptyAtThisTime)
+            throw new IllegalArgumentException("this room already have a session at this time period");
+    }
+
+    private void validateSessionDuration(CreateSessionRequest session) {
+        LocalDateTime sessionEndTime = session.getSessionEndTime();
+        LocalDateTime sessionTimePlusLimit = session.getSessionTime().plusHours(maxSessionHours);
+
+        Boolean exceedsSessionTimeLimit = sessionEndTime.isAfter(sessionTimePlusLimit);
+        if (exceedsSessionTimeLimit) {
+            throw new IllegalArgumentException("session has exceeded the time limit. if this is intentional and not an error, please contact the administrator");
+        }
+
+        LocalDateTime sessionTimePlusMinimum = session.getSessionTime().plusMinutes(minimumSessionMinutes);
+
+        Boolean lessThanMinimumSessionTimeAllowed = sessionEndTime.isBefore(sessionTimePlusMinimum);
+        if (lessThanMinimumSessionTimeAllowed) {
+            throw new IllegalArgumentException("session duration is less than the minimum required. please ensure the session duration is at least 10 minutes");
+        }
     }
 
     private List<Seat> createSeatsForSession(Integer totalSeats) {

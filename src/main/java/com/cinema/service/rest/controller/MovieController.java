7a7f96b7aaac7981d72692ffaaa4b040a55b3a9c
Renato <renaato.d.m@gmail.com>
2024-05-30 22:00:39 -0300

    feature: save images

--- src/main/java/com/cinema/service/rest/controller/MovieController.java
+++ src/main/java/com/cinema/service/rest/controller/MovieController.java
@@ -19,9 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
-import java.io.InputStream;
 
 @RestController
 @RequiredArgsConstructor
